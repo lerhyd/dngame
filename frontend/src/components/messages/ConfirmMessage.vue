80647633302eda2dbc791b029110f21d6ebbbc76
lerhyd <lerhyd@gmail.com>
2019-02-26 01:35:37 +0300

    fix front

---
+++ frontend/src/components/messages/ConfirmMessage.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <p>
+        Вам на почту должно было прийти сообщение. Перейдите по присланной ссылке, чтобы завершить аутентификацию.
+      </p>
+      <router-link to="/">Вернуться на главную страницу</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Confirm"
+    }
+</script>
+
+<style scoped>
+
+</style>

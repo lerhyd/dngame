7143611369eb1623a3075c762e780717ccad7ff3
CO8ECTb <dima210598@gmail.com>
2019-03-02 14:03:24 +0300

    Some fixes, added images for main page

--- frontend/src/components/main/MainPage.vue
+++ frontend/src/components/main/MainPage.vue
@@ -5,8 +5,8 @@
       <div class="main">
       <!--To use it you have to install library using command 'npm install sass-loader node-sass --save-dev'-->
       <!--And this one 'npm install vue-agile'-->
-          <agile :infinite="false" :dots="false" :arrows="false" :autoplay="true" :speed="1750"
-          :timing="'linear'" :fade="true" :pauseOnHover="false">
+          <agile :infinite="false" :dots="false" :arrows="false" :autoplay="true" :speed="1700"
+          :timing="'linear'" :fade="true" :pauseOnHover="false" :autoplaySpeed="5000">
             <div class="slide slide--1"><h3>slide 1</h3></div>
             <div class="slide slide--2"><h3>slide 2</h3></div>
             <div class="slide slide--3"><h3>slide 3</h3></div>
@@ -50,7 +50,7 @@
       size: cover;
 
     }
-    height: 500px;
+    height: 530px;
 
     &:before {
       background-color: rgba(#000, .2);
@@ -62,27 +62,26 @@
       width: 100%;
     }
 
-    &--1 {
-      background-image: url(../../assets/img/computer.png);
-      /*background: blue;*/
+    &--5 {
+      background-image: url(../../assets/img/slide5.png);
+    }
+
+    &--1{
+      background-image: url(../../assets/img/slide1.jpg);
     }
 
     &--2{
-      background: red;
+      background-image: url(../../assets/img/slide2.jpg);
     }
 
     &--3{
-      background: orange;
+      background-image: url(../../assets/img/slide3.jpg);
     }
 
     &--4{
-      background: green;
+      background-image: url(../../assets/img/slide4.jpg);
     }
-
-    &--5{
-      background: aqua;
-    }
-
+/*
     h3 {
       font-size: 32px;
       font-weight: 300;
@@ -92,6 +91,35 @@
       top: 50%;
       transform: translate(-50%, -50%);
     }
+    */
+  }
+
+
+
+  @media screen and (max-width: 700px){
+
+    .slide {
+      &--1 {
+        background-image: url(../../assets/img/mobSlide1.jpg);
+      }
+
+      &--2 {
+        background-image: url(../../assets/img/mobSlide2.jpg);
+      }
+
+      &--3 {
+        background-image: url(../../assets/img/mobSlide3.jpg);
+      }
+
+      &--4 {
+        background-image: url(../../assets/img/mobSlide4.jpg);
+      }
+
+      &--5 {
+        background-image: url(../../assets/img/mobSlide5.png);
+      }
+    }
+
   }
 
 

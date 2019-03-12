991155706eb6905d0ffc6029525c23a520e74f02
CO8ECTb <dima210598@gmail.com>
2019-03-13 02:01:01 +0300

    new table design

--- frontend/src/components/main/About.vue
+++ frontend/src/components/main/About.vue
@@ -9,15 +9,14 @@
           <div class="main-table">
           <table>
             <td id="first-row">
-              <tr>Правила игры</tr>
-              <tr>Правила Тетради Смерти</tr>
-              <tr>Что это вообще такое?</tr>
-              <tr>Кто вы такие?</tr>
-              <tr>А зачем?</tr>
+              <tr><button class="button" v-on:click="state = 1">Правила игры</button></tr>
+              <tr><button class="button" v-on:click="state = 2">Правила Тетради</button></tr>
+              <tr><button class="button" v-on:click="state = 3">Что это вообще такое?</button></tr>
+              <tr><button class="button" v-on:click="state = 4">Кто мы такие?</button></tr>
+              <tr><button class="button" v-on:click="state = 5">А зачем?</button></tr>
             </td>
             <td id="second-row">
               <tr colspan="5">
-                <a>3</a>
                 <a v-if="state == 5"> Хороший вопрос. Когда мы найдем на него ответ, то обязательно вам сообщим!</a>
               </tr>
             </td>
@@ -27,7 +26,6 @@
       </section>
       <section class="right-section">
       </section>
-      <button v-on:click="isActive = !isActive" >text</button>
     </div>
 </template>
 
@@ -46,6 +44,49 @@
 
 <style scoped>
 
+  .button{
+    width: 100%;
+    height: 60px;
+    background: transparent;
+    border: transparent;
+    /*border-bottom: 1px red solid;*/
+    outline: none;
+    font-family: 'Hand';
+    font-size: large;
+    letter-spacing: 0;
+    text-decoration: none;
+    /*text-transform: uppercase;*/
+    cursor: pointer;
+  }
+
+  .button:hover,
+  .button:active {
+    letter-spacing: 2px;
+    border-top: 1px solid #111111;
+    border-bottom: 1px solid #111111;
+    /*font-size: x-large;*/
+  }
+
+  .button:after,
+  .button:before {
+    transition: all 280ms ease-in-out;
+  }
+
+  .button:hover:after,
+  .button:hover:before {
+    backface-visibility: hidden;
+    border-color: #fff;
+    transition: width 350ms ease-in-out;
+    width: 70%;
+  }
+
+  .button:hover:before {
+    bottom: auto;
+    top: 0;
+    width: 70%;
+  }
+
+
   #first-row{
     width: 30%;
   }
@@ -55,14 +96,13 @@
 
   #app{
     font-family: 'Hand';
-    color: white;
   }
 
   .main-table{
     margin-top: 5px;
     margin-bottom: 5px;
     /*font-size: 130%;*/
-    width: 99%;
+    width: 96%;
     position: center;
     display: inline-block;
     padding: 0 10px;
@@ -76,30 +116,31 @@
     width: 100%;
     text-align: center;
     align: center;
-    border: 1px gray solid;
+    border: 1px #2d2d2d solid;
   }
 
   table, th, td {
-    /*border: 1px solid white;*/
+    border: 1px solid #2d2d2d;
   }
 
-  tr {
-    border-width: 1px;
-    border-style: solid;
-    border-image:
-      linear-gradient(
-        to left,
-        #585858,
-        rgba(255, 255, 255, 0)
-      ) 1 100%;
-    border-left: transparent;
-    border-right: transparent;
-  }
+  /*tr {*/
+    /*border-width: 1px;*/
+    /*border-style: solid;*/
+    /*border-image:*/
+      /*linear-gradient(*/
+        /*to left,*/
+        /*#585858,*/
+        /*rgba(255, 255, 255, 0)*/
+      /*) 1 100%;*/
+    /*border-left: transparent;*/
+    /*border-right: transparent;*/
+  /*}*/
 
   section{
     display: inline-block;
     width: 24%;
     text-align: center;
+    color: #111111;
     /*border: 1px gray solid;*/
   }
 
@@ -107,9 +148,11 @@
     display: inline-block;
     position: center;
     /*height: 90%;*/
-    width: 95%;
+    width: 99%;
     border: 5px solid rgb(254, 254, 254);
     border-radius: 30px;
+    background: rgb(255, 255, 255);
+
   }
 
   section img{width: 100%;}

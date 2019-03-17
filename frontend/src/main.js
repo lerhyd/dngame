8c3eebf6c69f98c1d01f25878e39bd5d64f2e309
lerhyd <lerhyd@gmail.com>
2019-03-17 21:45:46 +0300

    fix the issues

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,12 +5,10 @@ import App from './App'
 import router from './router'
 import Datetime from 'vue-datetime'
 import 'vue-datetime/dist/vue-datetime.css'
-<<<<<<< HEAD
+
 import { Settings } from 'luxon'
 
 Settings.defaultLocale = 'ru'
-=======
->>>>>>> 5394f71599c0d0d13dedba123fbd95e851a72227
 
 Vue.config.productionTip = false
 Vue.use(Datetime)

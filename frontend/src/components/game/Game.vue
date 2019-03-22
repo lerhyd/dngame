5c887a6a82fca6cbdc5de17f4f41ca464a88a22a
CO8ECTb <dima210598@gmail.com>
2019-03-22 22:29:21 +0300

    Added profile creation page

--- frontend/src/components/game/Game.vue
+++ frontend/src/components/game/Game.vue
@@ -1,9 +1,10 @@
 <template>
-  {{updateLoginName()}}
   <div>
+    <Header></Header>
+    {{updateLoginName()}}
     {{hasProfile()}}
     <header></header>
-    <section>
+    <section class="main-section">
       <div v-if="this.$store.getters.hasProfile">
         <div v-for="user in this.$store.getters.user">
           <p>{{user.login}} {{user.regDate}}</p>
@@ -21,41 +22,39 @@
       </div>
 
       <div v-if="!this.$store.getters.hasProfile">
-        <div class="main">
-          <form @submit.prevent="createProfile">
-            <label class="first_label">
-              Имя:
-              <input type="text" v-model="profile.name">
-            </label>
+          <div class="center-on-page">
+          <form id="profile-form" @submit.prevent="createProfile">
+            <h1>Создание профиля</h1>
+            <input class="form-profile-input" type="text" placeholder="Имя" v-model="profile.name" pattern="[A-Za-zА-Яа-яЁё]{1,20}" required>
             <br>
-            <label class="second_label">
-              Фамилия:
-              <input type="text" v-model="profile.surname">
-            </label>
+            <input class="form-profile-input" type="text" placeholder="Фамилия" v-model="profile.surname" pattern="[A-Za-zА-Яа-яЁё]{1,20}" required>
             <br>
-            <label class="third_label">
-              Отчество:
-              <input type="text" v-model="profile.patr">
-            </label>
+            <input class="form-profile-input" type="text" placeholder="Отчество" v-model="profile.patr" pattern="[A-Za-zА-Яа-яЁё]{1,20}">
             <br>
-            <label class="fourth_label">
-              Пол:
-              <select v-model="profile.sex">
-                <option disabled value="">Пожалуйста, выберите пол</option>
-                <option v-bind:value="true">Мужчина</option>
-                <option v-bind:value="false">Женщина</option>
-              </select>
-            </label>
             <br>
-            <label class="fifth_label">
-              Дата Рождения:
+            <input type="radio" id="man" v-bind:value="true" v-model="profile.sex" name="radio-group" required>
+            <label for="man">Мужчина</label>
+            <br>
+            <input type="radio" id="woman" v-bind:value="false" v-model="profile.sex" name="radio-group">
+            <label for="woman">Женщина</label>
+            <br>
+              <datetime
+                :phrases="{ok: 'Подтвердить', cancel: 'Отмена'}"
+                id="date-label" type="date"
+                v-bind:value="profile.bornDate"
+                class="red-theme"
+                :min-datetime="minDatetime"
+                :max-datetime="maxDatetime" auto
+              >
+                <label for="date-label" slot="before" id="birthday-label" >Дата Рождения:</label>
+              </datetime>
+            <br>
+            <button class="submit" type="submit" @click="updateLoginName();hasProfile();getUserInfo()">
+              <a>Создать</a>
+            </button>
 
-              <datetime v-model="profile.bornDate" type="datetime" format="yyyy-MM-dd HH:mm:ss"></datetime>
-            </label>
-            <br>
-            <input class="sub" type="submit" value="Создать" @click="updateLoginName();hasProfile();getUserInfo()"/>
           </form>
-        </div>
+          </div>
       </div>
     </section>
   </div>
@@ -63,17 +62,20 @@
 
 <script>
   import Header from "@/components/main/Header";
+  import { DateTime as LuxonDateTime } from 'luxon'
   export default {
     components: {Header},
     name: "Game",
     data () {
       return {
+        minDatetime: LuxonDateTime.local().minus({years: 100}).toISO(),
+        maxDatetime: LuxonDateTime.local().minus({ years: 18 }).toISO(),
         profile: {
           name: '',
           surname: '',
           patr: '',
-          sex: false,
-          bornDate: null
+          sex: true,
+          bornDate: null,
         }
       }
     },
@@ -120,11 +122,182 @@
   }
 </script>
 
-<style scoped>
+<style lang="scss" type="text/scss">
 
- * {
-   color: white;
- }
+  #birthday-label{
+    font-size: 20px;
+  }
+
+  .main-section{
+    width: 45%;
+    text-align: center;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    -moz-transform: translateX(-50%) translateY(-50%);
+    -webkit-transform: translateX(-50%) translateY(-50%);
+    transform: translateX(-50%) translateY(-50%);
+  }
+
+  .submit{
+    height: 40px;
+    display: inline;
+    width: 200px;
+    margin-top: 15px;
+    cursor: pointer;
+    font-family: 'Hand';
+    font-size: large;
+    color: #111111;
+    border-radius: 6px;
+    border: 1px solid #fdfefd;
+    user-focus: none;
+    background: whitesmoke;
+    outline: none;
+    margin-bottom: 30px;
+  }
+
+  .submit:hover a {
+    font-size: 17px;
+    mix-blend-mode: multiply;
+  }
+
+
+  /*#profile-form{*/
+    /*display: inherit;*/
+    /*padding-bottom: 5%;*/
+  /*}*/
+
+  .red-theme .vdatetime-popup__header,
+  .red-theme .vdatetime-calendar__month__day--selected > span > span,
+  .red-theme .vdatetime-calendar__month__day--selected:hover > span > span {
+    background: #880807;
+  }
+
+  .red-theme{
+    /*width: 500px;*/
+
+    font-family: 'Hand';
+    color: white;
+    user-focus: none;
+    background: none;
+    outline: none;
+    margin-top: 5px;
+    /*font-size: 20px;*/
+  }
+
+  html{
+    color: white;
+  }
+
+  .red-theme .vdatetime-year-picker__item--selected,
+  .red-theme .vdatetime-time-picker__item--selected,
+  .red-theme .vdatetime-popup__actions__button {
+    color: #880807;
+  }
+
+
+  [type="radio"]:checked,
+  [type="radio"]:not(:checked) {
+    position: absolute;
+    left: -9999px;
+  }
+  [type="radio"]:checked + label,
+  [type="radio"]:not(:checked) + label
+  {
+    position: relative;
+    padding-left: 30px;
+    cursor: pointer;
+    line-height: 25px;
+    display: inline-block;
+    margin-bottom: 7px;
+    margin-left: 5px;
+    font-size: 20px;
+  }
+  [type="radio"]:checked + label:before,
+  [type="radio"]:not(:checked) + label:before {
+    content: '';
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 18px;
+    height: 18px;
+    border: 1px solid #ddd;
+    border-radius: 100%;
+    background: #fff;
+  }
+  [type="radio"]:checked + label:after,
+  [type="radio"]:not(:checked) + label:after {
+    content: '';
+    width: 12px;
+    height: 12px;
+    background: #62000c;
+    position: absolute;
+    top: 4px;
+    left: 4px;
+    border-radius: 100%;
+    -webkit-transition: all 0.2s ease;
+    transition: all 0.2s ease;
+  }
+  [type="radio"]:not(:checked) + label:after {
+    opacity: 0;
+    -webkit-transform: scale(0);
+    transform: scale(0);
+  }
+  [type="radio"]:checked + label:after {
+    opacity: 1;
+    -webkit-transform: scale(1);
+    transform: scale(1);
+  }
+
+  .form-profile-input{
+    font-family: 'Hand';
+    color: white;
+    width: 270px;
+    height: 35px;
+    border-radius: .25em;
+    /*border: 1px solid white;*/
+    user-focus: none;
+    background: none;
+    outline: none;
+    margin-top: 10px;
+    font-size: 20px;
+    transition: all 0.5s linear;
+  }
+
+  .form-profile-input:hover{
+    background: rgb(222, 222, 222);
+    box-shadow: 0 0 300px 1px #c0c0c0;
+    /*border: 1px solid #474A59;*/
+    color: #111111;
+  }
+
+  .form-profile-input:hover::placeholder{
+    color: #111111;
+  }
+
+  ::placeholder {color: white;}
+
+  .form-profile-input::selection{
+    color: white;
+    background: #474A59;
+  }
+
+  .form-profile-input:focus::placeholder{
+    color: transparent;
+  }
+
+  .center-on-page{
+    font-family: 'Hand';
+    display: inline-block;
+    position: center;
+    height: 90%;
+    width: 60%;
+  }
+
+  h1 {
+    text-align: center;
+    font-size: xx-large;
+  }
 
   input {
     color: black;
--- frontend/src/components/game/Test.vue
+++ frontend/src/components/game/Test.vue
@@ -1,31 +1,238 @@
 <template>
-    <div>
-      <select>
-        <option v-for="item in items">{{item.name}}</option>
-      </select>
-    </div>
+
+  <div>
+    <Header></Header>
+    <section>
+      {{updateLoginName()}}
+      {{hasProfile()}}
+      {{getUserInfo()}}
+      <div v-if="this.$store.getters.hasProfile">
+        {{getProfile()}}
+        <div v-for="user in this.$store.getters.user">
+          <p>{{user.login}}</p>
+          <p>{{user.regDate}}</p>
+        </div>
+        <!--<div v-for="profile in this.$store.getters.profile">-->
+        <!--<p>{{profile.name}}</p>-->
+        <!--<p>{{profile.surname}}</p>-->
+        <!--<p>{{profile.patr}}</p>-->
+        <!--<p>{{profile.sex}}</p>-->
+        <!--<p>{{profile.bornDate}}</p>-->
+        <!--</div>-->
+        <button class="button16" @click="deleteProfile()" value="Удалить профиль"></button>
+        <br>
+        <router-link class="button16" to="/class">Играть</router-link>
+      </div>
+
+      <div v-if="!this.$store.getters.hasProfile">
+        <div class="center-on-page">
+          <form @submit.prevent="createProfile">
+            <h1>Выбери себе Имя</h1>
+            <div class="select">
+              <select id="selector">
+                <option class="name-options">༼つ◕_◕༽つ</option>
+                <option class="name-options" v-for="item in this.$store.getters.criminalPeople" v-bind="item.id">{{item.id}} {{item.surname}} {{item.name}} {{item.patr}}</option>
+              </select>
+            </div>
+            <datetime type="datetime" v-model="datetimeTheming" class="theme-orange"></datetime>
+            <input class="sub" type="submit" value="Создать" @click="updateLoginName();hasProfile()"/>
+            <!--<label class="first_label">-->
+            <!--Имя:-->
+            <!--<input type="text" v-model="profile.name">-->
+            <!--</label>-->
+            <!--<br>-->
+            <!--<label class="second_label">-->
+            <!--Фамилия:-->
+            <!--<input type="text" v-model="profile.surname">-->
+            <!--</label>-->
+            <!--<br>-->
+            <!--<label class="third_label">-->
+            <!--Отчество:-->
+            <!--<input type="text" v-model="profile.patr">-->
+            <!--</label>-->
+            <!--<br>-->
+            <!--<label class="fourth_label">-->
+            <!--Пол:-->
+            <!--<select v-model="profile.sex">-->
+            <!--<option disabled value="">Пожалуйста, выберите пол</option>-->
+            <!--<option v-bind:value="true">Мужчина</option>-->
+            <!--<option v-bind:value="false">Женщина</option>-->
+            <!--</select>-->
+            <!--</label>-->
+            <!--<br>-->
+            <!--<label class="fifth_label">-->
+            <!--Дата Рождения:-->
+
+            <!--<datetime v-model="profile.bornDate" type="datetime" format="yyyy-MM-dd HH:mm:ss"></datetime>-->
+            <!--</label>-->
+            <br>
+          </form>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Test",
-      data() {
-        return {
-          items: null,
-        }
+  import Header from "@/components/main/Header";
+  export default {
+    components: {Header},
+    name: "Test",
+    data () {
+      return {
+        id: null,
+      }
+    },
+    mounted: function() {
+      this.$store.dispatch('getRandomPeople').then();
+    },
+    methods: {
+      send () {
+        this.$store.dispatch('sendToken').then();
       },
-      methods: {
-        getPeople(){
-          this.$store.dispatch('getRandomPeople').then();
-          this.items = this.$store.getters.criminalPeople;
-        }
+      checkIfConfirmed() {
+        this.$store.dispatch('checkIfConfirmed').then();
       },
-      mounted() {
-        this.getPeople();
+      getUserInfo() {
+        this.$store.dispatch('getUser').then();
+      },
+      createProfile() {
+        this.$store.dispatch('createProfile', {
+          id: this.id,
+        }).then();
+      },
+      getProfile() {
+        this.$store.dispatch('getProfile').then();
+      },
+      deleteProfile() {
+        this.$store.dispatch('deleteProfile').then();
+      },
+      hasProfile() {
+        this.$store.dispatch('hasProfile').then();
+      },
+      updateLoginName() {
+        this.$store.dispatch('getLoginName').then();
       }
     }
+  }
 </script>
 
-<style scoped>
+<style lang="scss" type="text/scss">
+
+  .theme-orange .vdatetime-popup {
+    color: #440d0b;
+  }
+
+  .theme-orange {
+    color: grey;
+  }
+
+
+  .theme-orange .vdatetime-popup__header,
+  .theme-orange .vdatetime-calendar__month__day--selected > span > span,
+  .theme-orange .vdatetime-calendar__month__day--selected:hover > span > span {
+    background: #FF9800;
+    /*color: green;*/
+  }
+
+  .theme-orange .vdatetime-year-picker__item--selected,
+  .theme-orange .vdatetime-time-picker__item--selected,
+  .theme-orange .vdatetime-popup__actions__button {
+    color: #ff9800;
+    /*color: grey;*/
+  }
+
+
+  .center-on-page {
+    font-family: 'Hand';
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+  }
+  h1 {
+    text-align: center;
+  }
+
+  .name-options{
+    font-family: 'Hand';
+    font-size: xx-large;
+  }
+  /* Reset Select */
+  #selector {
+     -webkit-appearance: none;
+     -moz-appearance: none;
+     -ms-appearance: none;
+    appearance: none;
+    outline: 0;
+    box-shadow: none;
+    border: 0 !important;
+    background: #2c3e50;
+    background-image: none;
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0 0 0 .5em;
+    color: #fff;
+    cursor: pointer;
+  }
+  /* Custom Select */
+  .select {
+    position: relative;
+    display: block;
+    width: 20em;
+    height: 3em;
+    line-height: 3;
+    background: #2c3e50;
+    overflow: hidden;
+    border-radius: .25em;
+  }
+  #selector::-ms-expand {
+    display: none;
+  }
+  /* Arrow */
+  .select::after {
+    content: '\25BC';
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    padding: 0 1em;
+    background: #34495e;
+    pointer-events: none;
+  }
+  /* Transition */
+  .select:hover::after {
+    color: #62000c;
+  }
+  .select::after {
+     -webkit-transition: .25s all ease;
+     -o-transition: .25s all ease;
+    transition: .25s all ease;
+  }
+
+  input {
+    color: black;
+  }
+
+  .button16 {
+    display: inline-block;
+    text-decoration: none;
+    padding: 1em;
+    outline: none;
+    border-radius: 10px;
+    font-size: medium;
+    font-family: 'Hand';
+    color: white;
+  }
+  .button16:hover {
+    background-image:
+      radial-gradient(3px 45% at 0% 50%, rgba(255, 255, 255, 0.6), transparent),
+      radial-gradient(3px 45% at 100% 50%, rgba(255,255,255,.6), transparent);
+  }
+  .button16:active {
+    background-image:
+      radial-gradient(50% 50% at 50% 50%, rgba(74, 74, 74, 0.9), rgba(255,255,255,0));
+  }
 
 </style>

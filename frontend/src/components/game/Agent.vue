b4d90248330ca679e9a7d6269c7ecafb3f119396
CO8ECTb <dima210598@gmail.com>
2019-03-25 09:53:08 +0300

    Bug fixes

--- frontend/src/components/game/Agent.vue
+++ frontend/src/components/game/Agent.vue
@@ -32,8 +32,8 @@
       </section>
 
       <aside id="main-aside">
-        <div id="notebook-paper" v-if="isNoteOpen===true">
-          <div id="content">
+        <div id="tablet-paper" v-if="isNoteOpen===true">
+          <div id="tablet-content">
             <div v-if="isEntriesOpen===true">
               <div v-for="entry in this.$store.getters.entry">
                 <p>{{entry.personSurname}} {{entry.personName}}  {{entry.personPatr}} {{entry.success ? 'пойман':'не пойман'}}
@@ -41,10 +41,10 @@
               </div>
             </div>
             <div v-if="isEntryFormOpen===true">
-              Введите ФИО человека, которого хотите убить<br><br>
-              <input class="form-note-input" type="text" v-model="entryForm.victimSurname" placeholder="Фамилия человека" pattern="[A-Za-zА-Яа-яЁё]" required>
-              <input class="form-note-input" type="text" v-model="entryForm.victimName" placeholder="Имя человека" pattern="[A-Za-zА-Яа-яЁё]" required>
-              <input class="form-note-input" id="patr-field" type="text" v-model="entryForm.victimPatr" placeholder="Отчество человека" pattern="[A-Za-zА-Яа-яЁё]" required>
+              Введите ФИО человека, информацию по которому вы хотите запросить<br><br>
+              <input class="form-tablet-input" type="text" v-model="entryForm.victimSurname" placeholder="Фамилия человека" pattern="[A-Za-zА-Яа-яЁё]" required>
+              <input class="form-tablet-input" type="text" v-model="entryForm.victimName" placeholder="Имя человека" pattern="[A-Za-zА-Яа-яЁё]" required>
+              <input class="form-tablet-input" id="patr-field-tablet" type="text" v-model="entryForm.victimPatr" placeholder="Отчество человека" pattern="[A-Za-zА-Яа-яЁё]" required>
 
               <input type="radio" id="man" name="radio-group" v-model="entryForm.victimSex" v-bind:value="true" required>
               <label for="man">Мужчина</label>
@@ -59,8 +59,7 @@
               </div>
             </div>
             <div v-if="isFakeNewsOpen===true">
-              <input class="form-note-input" type="text" v-model="entryForm.desc" placeholder="Описание новости" required>
-              <br>
+              <input class="form-tablet-input" type="text" v-model="entryForm.desc" placeholder="Описание новости" required>
               <datetime
                 v-model="entryForm.deathDate"
                 :phrases="{ok: 'Подтвердить', cancel: 'Отмена'}"
@@ -70,90 +69,95 @@
                 :max-datetime="maxDatetime" auto>
                 <label for="date-label" slot="before">Дата и время публикации:</label>
               </datetime>
-              <br>
               <p class="select-form">Выберите действие:
-                <select class="form-control" v-model="entryForm.actionId" required>
+                <select class="form-control-tablet" v-model="entryForm.actionId" required>
                   <option v-for="action in this.$store.getters.action" v-bind:value="action.id">{{action.desc}}</option>
                 </select>
               </p>
               <p class="select-form">Выберите место действия:
-                <select class="form-control" v-model="entryForm.actionPlaceId" required>
+                <select class="form-control-tablet" v-model="entryForm.actionPlaceId" required>
                   <option v-for="actionPlace in this.$store.getters.actionPlace" v-bind:value="actionPlace.id">{{actionPlace.place}}</option>
                 </select>
               </p>
-              <p>Выберете регион, где всё произошло.</p>
-              <div class="select-custom">
-                <select class="form-control" v-model="commonRegion.continent">
-                  <option value="null" disabled>Континент</option>
-                  <option v-for="continent in this.$store.getters.continents" v-bind:value="continent">{{continent}}</option>
-                </select>
-              </div>
-              <div class="select-custom">
-                <select class="form-control" v-model="commonRegion.country" v-if="commonRegion.continent!=null">
-                  <option value="null" disabled>Страна</option>
-                  <option v-for="country in this.$store.getters.countries" v-bind:value="country">{{country}}</option>
-                </select>
-              </div>
-
-              <div class="select-custom">
-                <select class="form-control" v-model="commonRegion.city" v-if="commonRegion.country!=null">
-                  <option value="null" disabled>Город</option>
-                  <option v-for="city in this.$store.getters.cities" v-bind:value="city">{{city}}</option>
-                </select>
-              </div>
-              <p>Выберете регион, на который будут распространяться новости.</p>
-              <div class="select-custom">
-                <select class="form-control" v-model="distRegion.continent">
-                  <option value="1" >Континент</option>
+              <p>Выберете регион, где всё произошло.
+              <select class="form-control-tablet" v-model="commonRegion.continent">
+                <option value="null" disabled>Континент</option>
+                <option v-for="continent in this.$store.getters.continents" v-bind:value="continent">{{continent}}</option>
+              </select>
+              <select class="form-control-tablet" v-model="commonRegion.country" v-if="commonRegion.continent!=null">
+                <option value="null" disabled>Страна</option>
+                <option v-for="country in this.$store.getters.countries" v-bind:value="country">{{country}}</option>
+              </select>
+              <select class="form-control-tablet" v-model="commonRegion.city" v-if="commonRegion.country!=null">
+                <option value="null" disabled>Город</option>
+                <option v-for="city in this.$store.getters.cities" v-bind:value="city">{{city}}</option>
+              </select>
+              </p>
+              <p>Выберете регион, на который будут распространяться новости.
+                <br>
+                <select class="form-control-tablet" v-model="distRegion.continent">
+                  <option value="null" >Континент</option>
                   <option v-for="continent in this.$store.getters.distContinents" v-bind:value="continent">{{continent}}</option>
                 </select>
-                <select class="form-control" v-model="distRegion.country" v-if="distRegion.continent!=null">
+                <select class="form-control-tablet" v-model="distRegion.country" v-if="distRegion.continent!=null">
                   <option value="null" >Страна</option>
                   <option v-bind:value="0">Нет</option>
                   <option v-for="country in this.$store.getters.distCountries" v-bind:value="country">{{country}}</option>
                 </select>
-                <select class="form-control" v-model="distRegion.city" v-if="distRegion.country!=null">
+                <select class="form-control-tablet" v-model="distRegion.city" v-if="distRegion.country!=null">
                   <option value="null" >Город</option>
                   <option v-bind:value="0">Нет</option>
                   <option v-for="city in this.$store.getters.distCities" v-bind:value="city">{{city}}</option>
                 </select>
+              </p>
+              <p>Выберите виновного:
+              <select class="form-control-tablet" v-model="newsForm.guiltyPersonId">
+                <option v-for="person in this.$store.getters.persons" v-bind:value="person.id">
+                  {{person.surname}} {{person.name}} {{person.patr}}
+                </option>
+              </select>
+              </p>
+              <p v-if="newsForm.guiltyPersonId !== 0">Выберите пострадавшего(не обязательно).</p>
+              <div v-if="newsForm.guiltyPersonId !== 0">
+                <select class="form-control-tablet" v-model="newsForm.victimId">
+                  <option v-for="person in this.$store.getters.victimPersons" v-bind:value="person.id">
+                    {{person.name}} {{person.surname}} {{person.patr}}
+                  </option>
+                </select>
               </div>
-
-
-
+              <button class="form-tablet-input" @click="createFakeNews();closeFakeNews();clearNews()">Создать новость</button>
             </div>
           </div>
         </div>
       </aside>
 
+    <div v-if="this.$store.getters.matchEnded">
+      <div v-if="this.$store.getters.isKira">
+        <div v-if="this.$store.getters.isKiraWin && !this.$store.getters.isGameDrawn">
+          Вы победили.
+        </div>
+        <div v-if="!this.$store.getters.isKiraWin && !this.$store.getters.isGameDrawn">
+          Вы проиграли.
+        </div>
+        <div v-if="this.$store.getters.isGameDrawn">
+          Ничья.
+        </div>
+      </div>
+      <div v-if="!this.$store.getters.isKira">
+        <div v-if="!this.$store.getters.isKiraWin && !this.$store.getters.isGameDrawn">
+          Вы победили.
+        </div>
+        <div v-if="this.$store.getters.isKiraWin && !this.$store.getters.isGameDrawn">
+          Вы проиграли.
+        </div>
+        <div v-if="this.$store.getters.isGameDrawn">
+          Ничья.
+        </div>
+      </div>
+      <button class="button16" @click="exit()">Вернутся в меню выбора</button>
+    </div>
 
-    <!--Errors-->
-  <div v-if="this.$store.getters.matchEnded">
-    <div v-if="this.$store.getters.isKira">
-      <div v-if="this.$store.getters.isKiraWin && !this.$store.getters.isGameDrawn">
-        Вы победили.
-      </div>
-      <div v-if="!this.$store.getters.isKiraWin && !this.$store.getters.isGameDrawn">
-        Вы проиграли.
-      </div>
-      <div v-if="this.$store.getters.isGameDrawn">
-        Ничья.
-      </div>
-    </div>
-    <div v-if="!this.$store.getters.isKira">
-      <div v-if="!this.$store.getters.isKiraWin && !this.$store.getters.isGameDrawn">
-        Вы победили.
-      </div>
-      <div v-if="this.$store.getters.isKiraWin && !this.$store.getters.isGameDrawn">
-        Вы проиграли.
-      </div>
-      <div v-if="this.$store.getters.isGameDrawn">
-        Ничья.
-      </div>
-    </div>
-    <button class="button16" @click="exit()">Вернутся в меню выбора</button>
   </div>
-</div>
 </template>
 
 <script>
@@ -184,8 +188,8 @@
         },
         newsForm: {
           distRegionId: null,
-          victimId: null,
-          guiltyPersonId: null
+          victimId: 0,
+          guiltyPersonId: 0
         },
         commonRegion: {
           regionId: null,
@@ -246,24 +250,25 @@
       },
       openEntryForm(){
         this.isEntryFormOpen = true
-        this.isEntriesOpen = false
         this.isFakeNewsOpen = false
+        this.isEntriesOpen = false
       },
       closeEntryForm(){
         this.isEntryFormOpen = false
       },
       openEntries(){
         this.isEntriesOpen = true
-        this.isFakeNewsOpen = false
         this.isEntryFormOpen = false
+        this.isFakeNewsOpen = false
+
       },
       closeEntries(){
         this.isEntriesOpen = false
       },
       openFakeNews(){
         this.isFakeNewsOpen = true
-        this.isEntriesOpen = false
         this.isEntryFormOpen = false
+        this.isEntriesOpen = false
       },
       closeFakeNews(){
         this.isFakeNewsOpen = false
@@ -340,7 +345,14 @@
       getPersons(){
         this.$store.dispatch('getPersons',
           {
-            usedPersonId: this.newsForm.guiltyPersonId
+            usedPerson: this.newsForm.guiltyPersonId
+          }
+        )
+      },
+      getVictimPersons(){
+        this.$store.dispatch('getVictimPersons',
+          {
+            usedPerson: this.newsForm.guiltyPersonId
           }
         )
       },
@@ -397,7 +409,7 @@
         this.newsForm.commonRegionId = null
         this.newsForm.distRegionId = null
         this.newsForm.victimId = null
-        this.newsForm.guiltyPersonId = null
+        this.newsForm.guiltyPersonId = 0
       }
     },
     mounted() {
@@ -440,11 +452,11 @@
       'distRegion.city': function (val) {
         console.log('')
         this.getFullRegionId()
+      },
+      'newsForm.guiltyPersonId': function (val) {
+        this.getVictimPersons()
       }
     }
-
-
-
   }
 </script>
 
@@ -459,10 +471,12 @@
   .select-form{
     display: block;
   }
-  .form-control{
+  .form-control-tablet{
     border: transparent;
-    margin-bottom: 5px;
+    /*margin-bottom: 5px;*/
+    margin-right: 10px;
     font-family:helvetica neue, helvetica, arial, sans-serif;
+    font-weight: 300;
     color: black;
     font-size: 20px;
     user-focus: none;
@@ -521,49 +535,32 @@
   }
 
 
-  #notebook-paper {
-    -webkit-box-sizing:border-box;
-    -moz-box-sizing:border-box;
-    -ms-box-sizing:border-box;
-    -o-box-sizing:border-box;
-    box-sizing:border-box;
+  #tablet-paper {
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    -ms-box-sizing: border-box;
+    -o-box-sizing: border-box;
+    box-sizing: border-box;
     background: #f1f1f1;
-    font-family:helvetica neue, helvetica, arial, sans-serif;
-    font-weight:200;
-    width:95%;
-    height:600px;
-    background: linear-gradient(to bottom,white 29px,#00b0d7 1px);
-    margin:0px auto;
-    background-size: 100% 30px;
-    position:relative;
-    padding-left:8%;
-    padding-right:20px;
+    font-family: helvetica neue, helvetica, arial, sans-serif;
+    font-weight: 200;
+    width: 95%;
+    height: 600px;
+    margin: 0px auto;
+    position: relative;
+    padding-left: 8%;
+    padding-right: 20px;
     overflow-y: scroll;
-    border-radius:5px;
-    -webkit-box-shadow:3px 3px 3px rgba(0,0,0,.2),0px 0px 6px rgba(0,0,0,.2);
-    -moz-box-shadow:3px 3px 3px rgba(0,0,0,.2),0px 0px 6px rgba(0,0,0,.2);
-    -ms-box-shadow:3px 3px 3px rgba(0,0,0,.2),0px 0px 6px rgba(0,0,0,.2);
-    -o-box-shadow:3px 3px 3px rgba(0,0,0,.2),0px 0px 6px rgba(0,0,0,.2);
-    box-shadow:3px 3px 3px rgba(0,0,0,.2),0px 0px 6px rgba(0,0,0,.2);}
-
-  #notebook-paper:before {
-    content:'';
-    display:block;
-    position:absolute;
-    z-index:1;
-    top:0;
-    left:7%;
-    height:100%;
-    width:1px;
-    background:#db4034;
+    border-radius: 50px;
+    border: 30px dimgrey solid;
   }
 
-  #content {
+  #tablet-content {
     margin-top:5px;
     font-size:20px;
     line-height:30px;}
 
-  #content p {
+  #tablet-content p {
     margin:0 0 30px 0;
   }
 
@@ -585,7 +582,7 @@
     background-repeat: no-repeat;
   }
 
-  .form-note-input{
+  .form-tablet-input{
     border: transparent;
     display: block;
     width: 100%;
@@ -599,7 +596,7 @@
     /*font-weight: lighter;*/
   }
 
-  .form-note-input::placeholder{
+  .form-tablet-input::placeholder{
     font-size: 20px;
     font-family:helvetica neue, helvetica, arial, sans-serif;
     color: black;
@@ -683,11 +680,7 @@
     transform: scale(1);
   }
 
-  #patr-field{
-    margin-bottom: 0px;
-  }
-
-  #death-description{
+  #patr-field-tablet{
     margin-bottom: 0px;
   }
 
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -12,7 +12,7 @@ import NotFound from '@/components/main/NotFound'
 import About from '@/components/main/About'
 import NotConfirmEmailMessage from '@/components/messages/NotConfirmEmailMessage'
 import Class from '@/components/game/Class'
-import Test from '@/components/game/Test'
+import Kira from '@/components/game/Kira'
 import FindMatch from '@/components/game/FindMatch'
 import Area from '@/components/game/Area'
 import Achievements from '@/components/game/Achievements'
@@ -76,9 +76,9 @@ export default new Router({
       component: Game
     },
     {
-      path: '/test',
-      name: 'Test',
-      component: Test
+      path: '/kira',
+      name: 'Kira',
+      component: Kira
     },
     {
       path: '/confirm',
